db6591ccfda5732ddf2b8088133046e28b385232
Zikrovat <18535846+Zikrovat@users.noreply.github.com>
2020-03-19 09:07:22 -0300

    Stone_0.1

---
+++ Assets/_Stone/Scripts/Global/GlobalUtilitys.cs
@@ -0,0 +1,24 @@
+﻿namespace _Stone.Status
+{
+    [System.Serializable]
+    public struct BasicStatus
+    {
+        public int Heakth;
+
+
+        public BasicStatus(int value)
+        {
+            Heakth = value;
+
+        }
+
+    }
+}
+namespace _Stone.MasterHouse
+{
+    public interface IMasterHouse
+    {
+         void TakeDamage(int amout);
+        
+    }
+}
---
+++ Assets/_Stone/Scripts/MasterJHoise/MasterHouse.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace _Stone.MasterHouse
+{
+    public class MasterHouse : MonoBehaviour,IMasterHouse
+    {
+        [SerializeField] private _Stone.Status.BasicStatus HouseStatus;
+
+        public void TakeDamage(int amout)
+        {
+
+            HouseStatus.Heakth -= amout;
+            Debug.Log($"enemy did {amout} damage");
+            if (HouseStatus.Heakth <= 0)
+            {
+                DieFunction();
+                HouseStatus.Heakth = 0;
+                //die
+
+            }
+
+        }
+
+
+        private void DieFunction()
+        {
+            Debug.Log($"{transform.name} just die");
+        }
+    }
+
+}
+
