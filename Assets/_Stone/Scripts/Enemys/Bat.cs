aff84634d2e3d2e18284d67fdb6f7f7cdc301810
Zikrovat <18535846+Zikrovat@users.noreply.github.com>
2020-03-19 21:51:31 -0300

    _Stone 0.1

--- Assets/_Stone/Scripts/Enemys/Bat.cs
+++ Assets/_Stone/Scripts/Enemys/Bat.cs
@@ -1,35 +1,106 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Stone.Enemy.Bat
 {
-    public class Bat : EnemyBase.EnemyBase
+    public class Bat : _Stone.Creature.CreatureBase,IBat
     {
-        [SerializeField] private _Stone.Status.BasicStatus status;
-        [SerializeField] private float MoveSpeed;
-        [SerializeField] private int BaseDamageAmount = 1;
 
+        [SerializeField] private List<_Stone.Item.ItemBase> ItemsDrop = new List<Item.ItemBase>();
+        [SerializeField] private float DropAmount;
+
+        private Dictionary<string, Spells.spell> AttackSpellsList = new Dictionary<string, Spells.spell>();
+        [SerializeField] private List<Spells.spell> _SpellList_ = new List<Spells.spell>();
+        private void Start()
+        {
+            for (int i = 0; i < _SpellList_.Count; i++)
+            {
+                AttackSpellsList.Add(_SpellList_[i].AttackName, _SpellList_[i]);
+
+            }
+        }
         private void FixedUpdate()
         {
 
-            transform.Translate(Vector2.left * MoveSpeed * Time.deltaTime);
+          
+            if (Vector3.Distance(transform.position,_Stone.MasterHouse.MasterHouse.instance.transform.position) < 5)
+            {
+                Target = _Stone.MasterHouse.MasterHouse.instance.transform;
+    
+            }
         
+            if (Target)
+            {
+                AIFunction();
+            }
+        }
+        private void AIFunction()
+        {
+
+
+            if (Vector3.Distance(Target.transform.position, transform.position) < 1.2f)
+            {
+                transform.LookAt(Target);
+                if (AninControler)
+                {
+
+                }
+                if (!AttackSpellsList["BitAttack"].IsCasting)
+                {
+                    BitAttack();
+                }
+            }
+            else
+            {
+                if (Vector3.Distance(Target.transform.position, transform.position) > 1.2f)
+                {
+                    if (!CombatMaster.CurrentSpell.IsCasting)
+                    {
+
+                        transform.Translate(Vector2.left * MoveSpeed * Time.deltaTime);
+                        transform.LookAt(Target);
+
+                    }
+                }
+
+
+            }
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnDestroy()
         {
-            Debug.Log(collision.transform.name);
-            _Stone.MasterHouse.MasterHouse house = collision.transform.GetComponent<_Stone.MasterHouse.MasterHouse>();
-            if (house)
+            for (int i = 0; i < DropAmount; i++)
             {
-                house.TakeDamage(BaseDamageAmount);
-                Destroy(gameObject);
+                float value = (UnityEngine.Random.Range(0, 100) / ItemsDrop.Count) * 100;
+                DropItem(ItemsDrop[UnityEngine.Random.Range(0, ItemsDrop.Count)]);
             }
 
+
+
+        }
+        void DropItem(_Stone.Item.ItemBase item)
+        {
+            GameObject SpawnItem = Instantiate(item?.PrefabModel);
         }
 
+        public void SonicWave()
+        {
+            MoveSpeed = 1;
+            CombatMaster.CallSpell(AttackSpellsList["SonicWave"]);
+            Destroy(gameObject,0.3f);
+            MoveSpeed = 3;
+        }
 
+        public void BitAttack()
+        {
+            MoveSpeed = 5;
+            CombatMaster.CallSpell(AttackSpellsList["BitAttack"]);
+            Destroy(gameObject,0.3f);
+            MoveSpeed = 3;
+
+        }
     }
 }
 
---
+++ Assets/_Stone/Scripts/MasterHose/MasterHouse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace _Stone.MasterHouse
+{
+    public class MasterHouse : Creature.CreatureBase
+    {
+        public static MasterHouse instance { get; private set; }
+        [SerializeField] private int Score = 0;
+        private void Update()
+        {
+            transform.Translate(Vector2.right * MoveSpeed * Time.deltaTime);
+        }
+        private void Start()
+        {
+            instance = this;
+        }
+
+        public void AddScore(int Amount)
+        {
+            Score += Amount;
+        }
+
+
+
+
+    }
+
+}
+
